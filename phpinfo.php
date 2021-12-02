b36ccaed94fb0657dbdd041f209dceaf96d6ba92
eduja5979 <edu-jaramillo@nogales.edu.co>
2021-12-01 21:23:17 -0500

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -3,25 +3,28 @@
 
 <?php 
 $ay = $_GET["type"];
-echo $_GET["type"]; ?>
+echo $ay; 
+?>
 
 <?php
-$servername = "sql10.freesqldatabase.com";
-$username = "sql10453225";
-$password = "w8smxnPdRG";
+
+$servername = "remotemysql.com";
+$username = "VLdtphsizV";
+$password = "7h68oQ4XhX";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
+
+
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 
-$sql = "CREATE TABLE table1 (con CHAR(500))";
 
-$isql = "INSERT INTO table1 VALUES ('$ay')";
+
+$sql = "INSERT INTO table1 (cont) VALUES ('$ay')";
     
     if ($conn->query($sql) === TRUE) {
       echo "Success";
@@ -29,13 +32,13 @@ $isql = "INSERT INTO table1 VALUES ('$ay')";
       echo "Error: " . $conn->error;
     }
     
-    $sql = "SELECT con FROM table1";
+    $sql = "SELECT cont FROM table1";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
      // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo $row["con"]. "<br>";
+            echo $row["cont"]. "<br>";
         }
     } 
     else {
