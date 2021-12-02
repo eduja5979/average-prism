50ccb7818ff8ad1aa97597fbbedc78c67231756f
eduja5979 <edu-jaramillo@nogales.edu.co>
2021-12-02 16:12:51 -0500

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,20 +1,20 @@
 <html>
 <body>
 
-<?php 
-$ay = $_GET["type"];
-echo $ay; 
-?>
 
 <?php
+$ay = $_GET["type"];
+echo $ay; 
+
 
 $servername = "remotemysql.com";
 $username = "VLdtphsizV";
 $password = "7h68oQ4XhX";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $username);
+echo "<\br>";
+echo "<\br>";
 
 
 // Check connection
@@ -26,11 +26,11 @@ if ($conn->connect_error) {
 
 $sql = "INSERT INTO table1 (cont) VALUES ('$ay')";
     
-    if ($conn->query($sql) === TRUE) {
+   /* if ($conn->query($sql) === TRUE) {
       echo "Success";
     } else {
       echo "Error: " . $conn->error;
-    }
+    }*/
     
     $sql = "SELECT cont FROM table1";
     $result = $conn->query($sql);
@@ -39,6 +39,8 @@ $sql = "INSERT INTO table1 (cont) VALUES ('$ay')";
      // output data of each row
         while($row = $result->fetch_assoc()) {
             echo $row["cont"]. "<br>";
+            echo "<\br>";
+            echo "<\br>";
         }
     } 
     else {
